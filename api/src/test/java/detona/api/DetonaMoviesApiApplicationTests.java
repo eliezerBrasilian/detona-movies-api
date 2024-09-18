0a737cef996a00e6e2e6a4c89a4b30460fcb0d57
eliezerBrasilian <eliezerassuncaocustodio@hotmail.com>
2024-09-18 19:26:41 -0300

    deploy

---
+++ api/src/test/java/detona/api/DetonaMoviesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package detona.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetonaMoviesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
